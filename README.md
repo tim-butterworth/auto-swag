3e0b00694fac96449de94ad73023e4a32564471c
Tim <timothy.d.butterworth@gmail.com>
2019-06-17 12:58:15 -0600

    Adds readme and better configuration

---
+++ README.md
@@ -0,0 +1,35 @@
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [Official Gradle documentation](https://docs.gradle.org)
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
+* [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
+* [Building REST services with Spring](https://spring.io/guides/tutorials/bookmarks/)
+
+### Additional Links
+These additional references should also help you:
+
+* [Gradle Build Scans – insights for your project's build](https://scans.gradle.com#gradle)
+
+
+
+
+## Starting the app
+
+# Without swagger
+
+- `./gradlew bootRun`
+
+# With swagger
+
+- `SPRING_PROFILES_ACTIVE=swagger ./gradlew bootRun`
+
+[swagger](http://localhost:8080/swagger-ui.html)
+
+additional documentation can be found [here](http://springfox.github.io/springfox/docs/current/)
--- src/main/java/com/example/demo/configuration/swagger/SwaggerConfiguration.java
+++ src/main/java/com/example/demo/configuration/swagger/SwaggerConfiguration.java
@@ -1,21 +1,18 @@
 package com.example.demo.configuration.swagger;
 
-import com.example.demo.controllers.ControllerOne;
-import com.example.demo.controllers.ControllerTwo;
 import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.schema.WildcardType;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,15 +25,11 @@ import static springfox.documentation.schema.AlternateTypeRules.newRule;
 
 @Configuration
 @EnableSwagger2
-@ComponentScan(basePackageClasses = {
-        ControllerOne.class,
-        ControllerTwo.class
-})
 @Profile("swagger")
 public class SwaggerConfiguration {
     @Bean
     public Docket petApi(TypeResolver typeResolver) {
-        return new Docket(DocumentationType.SWAGGER_12)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
@@ -45,16 +38,32 @@ public class SwaggerConfiguration {
                 .directModelSubstitute(LocalDate.class, String.class)
                 .genericModelSubstitutes(ResponseEntity.class)
                 .alternateTypeRules(
-                        newRule(typeResolver.resolve(DeferredResult.class,
-                                typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
-                                typeResolver.resolve(WildcardType.class)))
+                        newRule(
+                                typeResolver.resolve(
+                                        DeferredResult.class,
+                                        typeResolver.resolve(ResponseEntity.class, WildcardType.class)
+                                ),
+                                typeResolver.resolve(WildcardType.class)
+                        )
+                )
                 .useDefaultResponseMessages(false)
-                .globalResponseMessage(RequestMethod.GET,
-                        singletonList(new ResponseMessageBuilder()
-                                .code(500)
-                                .message("500 message")
-                                .responseModel(new ModelRef("Error"))
-                                .build()))
+                .globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                        .name("AUTHENTICATION")
+                                        .description("An authentication header!")
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(true)
+                                        .build(),
+                                new ParameterBuilder()
+                                        .name("X-SOMETHING-SOMETHING")
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(true)
+                                        .build()
+                        )
+                )
                 .enableUrlTemplating(true);
     }
 }
